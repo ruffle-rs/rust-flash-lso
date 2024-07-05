54b5a5912f23fc8f57604d81330c6356b3f682e2
CUB3D <callumthom11@gmail.com>
2024-07-05 14:51:51 +0100

    Fix amf3 fuzz cases

--- fuzz/fuzz_targets/fuzz_amf3_int_unsigned.rs
+++ fuzz/fuzz_targets/fuzz_amf3_int_unsigned.rs
@@ -1,8 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use amf::amf3;
+use flash_lso::amf3::read::fuzz_read_int;
 
 fuzz_target!(|data: &[u8]| {
-    amf3::read_int(data);
+    let _ = fuzz_read_int(data);
 });
